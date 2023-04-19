82abd9b2c1f1cebf9ae32e059df03fd7efe7095a
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-04-19 23:40:33 +0530

    Day 1 task added

---
+++ day1/intro.js
@@ -0,0 +1,17 @@
+console.log("Hello World") // Comments make the code readable
+console.log("30DaysOfJS") // Welcome to 30DaysOfJavascript
+/* comments can make code readable, 
+easy to reuse and informative */
+
+
+console.log(typeof 5 );
+
+
+let firstName;
+let lastName;
+let age;
+
+age = 15;
+firstName = "Nikhil";
+lastName = "Mishra";
+console.log( firstName, age , lastName);
---
+++ day1/variable.js
@@ -0,0 +1,14 @@
+let name = "Nikhil";
+let god = "True";
+let num = 5;
+
+console.log(typeof 5 );
+
+let firstName;
+let lastName;
+let age;
+
+age = 15;
+firstName = Nikhil;
+lastName = Mishra;
+console.log( firstName, age , lastName);
