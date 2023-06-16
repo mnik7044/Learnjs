a5561bfd9564079252ec0df893e5d97fa70380d9
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-06-16 20:56:37 +0530

    learned about template literals in string

---
+++ template_literals/index.js
@@ -0,0 +1,13 @@
+const another = ` This is my first message
+ using template literals`
+
+ const name = "godnik"
+
+ const anemail = `This is an email
+ this is for you
+ your ${name}`
+
+ console.log(anemail)
+
+
+ ${Here we can add any kind of expressions variable or things like that this is a place holder, we can even call a function that reruns value}
